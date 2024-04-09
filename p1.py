ba939e1223338f53c5fe192b1ba7d2bfb445fdda
michael-mimi <michenfei36@hotmail.com>
2024-04-10 00:13:51 +0800

    hangman

--- p1.py
+++ p1.py
@@ -19,17 +19,17 @@
 
 # print(repr(list[int]))
 
-import customtkinter
+# import customtkinter
 
-def button_callback():
-    print("button pressed")
+# def button_callback():
+#     print("button pressed")
 
-app = customtkinter.CTk()
-app.title("my app")
-app.geometry("400x150")
-app.grid_columnconfigure(0,weight=1)
+# app = customtkinter.CTk()
+# app.title("my app")
+# app.geometry("400x150")
+# app.grid_columnconfigure(0,weight=1)
 
-button = customtkinter.CTkButton(app, text="my button", command=button_callback)
-button.grid(row=0,column=0,padx=20,pady=20,sticky="ew")
+# button = customtkinter.CTkButton(app, text="my button", command=button_callback)
+# button.grid(row=0,column=0,padx=20,pady=20,sticky="ew")
 
-app.mainloop()
+# app.mainloop()
